f47500ff115d7d6c8196ea7f50613575294bc9d1
Saeed Ahmed <saeedchachar987654@gmail.com>
2021-10-30 14:07:35 +0500

    last

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../Models/userModel');
+const User = require('../models/userModel');
 var bcrypt = require('bcryptjs');
 const config = require('../config/keys');
 const jwt = require('jsonwebtoken');
